848ee66d540daa038ea69d056095dc1d1fd985ca
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-07-14 09:14:59 -0400

    Update propcontrol.py

--- propcontrol.py
+++ propcontrol.py
@@ -1,23 +1,5 @@
-
-
-class Solution:    
-    def propcontrol(self, center, res):
-            #type center: int
-            #type res: tuples of int
-            #return type: float
-            
-            #TODO: Write code below to return a float with the solution to the prompt.
-            return (center) / (res[0]) * (2)-1
-
 def main():
-    center = int(input().strip())
-    resx = int(input().strip())
-    resy = int(input().strip())
-    res = (resx, resy)
-
-    tc1 = Solution()
-    ans = tc1.propcontrol(center, res)
-    ans=format(ans, ".6f")
+    ans=format((int(input().strip())) / (int(input().strip())) * (2)-1, ".6f")
     print(ans)
 
 if __name__ and "__main__":
