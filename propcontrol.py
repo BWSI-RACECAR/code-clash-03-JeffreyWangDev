982f58f5a4b8ef295d8ef909b6bcf45a6fcf1942
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-07-14 09:17:19 -0400

    Update propcontrol.py

--- propcontrol.py
+++ propcontrol.py
@@ -1,6 +1,1 @@
-def main():
-    ans=format((int(input().strip())) / (int(input().strip())) * (2)-1, ".6f")
-    print(ans)
-
-if __name__ and "__main__":
-    main()
+main = lambda : format((int(input().strip())) / (int(input().strip())) * (2)-1, ".6f")
